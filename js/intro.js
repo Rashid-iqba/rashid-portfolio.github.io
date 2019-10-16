3196b6afa04f56657acb8ac3c4f39807321e8e5a
RASHID IQBAL <45360835+Rashid-iqba@users.noreply.github.com>
2019-10-16 08:34:43 +0530

    new entry

---
+++ js/intro.js
@@ -0,0 +1,10 @@
+
+
+	document.write("phir kabhi.....</hr>");
+	document.write("channa mere ya.....</hr>");
+document.write("kal ho na ho...</hr>");
+document.write("something is batter then nothings</hr>");
+
+
+
+
